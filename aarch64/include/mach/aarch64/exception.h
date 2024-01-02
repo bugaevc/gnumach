9e300f00160b0514cda9c7ad691b9d540489162b
Sergey Bugaev <bugaevc@gmail.com>
2024-01-02 14:43:23 +0300

    Add basic public headers for AArch64
    And just enough of buildsystem plumbing to install them.
    
    Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>

---
+++ aarch64/include/mach/aarch64/exception.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2023-2024 Free Software Foundation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
+ *	Codes and subcodes for AArch64 exceptions.
+ */
+#ifndef	_MACH_AARCH64_EXCEPTION_H_
+#define _MACH_AARCH64_EXCEPTION_H_
+
+
+/*
+ *	EXC_BAD_INSTRUCTION
+ */
+
+
+/*
+ *	EXC_ARITHMETIC
+ */
+
+#define EXC_AARCH64_FP_ID		1	/* input denormal, IDF bit */
+#define EXC_AARCH64_FP_IX		2	/* inexact, IXF bit */
+#define EXC_AARCH64_FP_UF		3	/* underflow, UFF bit */
+#define EXC_AARCH64_FP_OF		4	/* overflow, OFF bit */
+#define EXC_AARCH64_FP_DZ		5	/* divide by zero, DZF bit */
+#define EXC_AARCH64_FP_IO		6	/* invalid operation, IOF bit */
+
+/*
+ *	EXC_SOFTWARE
+ */
+
+/*
+ *	EXC_BAD_ACCESS
+ *
+ *	Exception code normally holds a kern_return_t value (such as
+ *	KERN_INVALID_ADDRESS), but for AArch64-specific exceptions these
+ *	values can be used as exception code instead; they must not conflict
+ *	with kern_return_t values.
+ */
+#define EXC_AARCH64_AL_FAULT		100	/* alignment fault */
+#define EXC_AARCH64_AL_PC		101	/* misaligned pc */
+#define EXC_AARCH64_AL_SP		102	/* misaligned sp */
+#define EXC_AARCH64_PAC			103	/* PAC failure */
+#define EXC_AARCH64_MTE			104	/* MTE failure */
+#define EXC_AARCH64_BTI			105	/* BTI failure */
+
+/*
+ *	EXC_BREAKPOINT
+ */
+
+#define EXC_AARCH64_BRK			1	/* BKPT, BRK */
+
+#endif	/* _MACH_AARCH64_EXCEPTION_H_ */
